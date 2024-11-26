2bdf18a872e4c9e4957b7bc09c912a2a27e14ce7
novbo1 <154607823+novbo1@users.noreply.github.com>
2024-11-26 11:52:16 +0800

    OOPtest3 upload

---
+++ OOPtest3/src/BlueDrug.java
@@ -0,0 +1,4 @@
+
+public class BlueDrug extends Drug{
+	
+}
---
+++ OOPtest3/src/Drug.java
@@ -0,0 +1,45 @@
+
+public class Drug {
+	private String size;
+	private int addLife;
+	private int addMagic;
+	
+	public Drug() {}
+	
+	public Drug(String size) {
+		this.size = size;
+		
+		switch (size) {
+        case "Large":
+            this.addLife = 120;
+            this.addMagic = 100;
+            break;
+        case "Medium":
+            this.addLife = 80;
+            this.addMagic = 60;
+            break;
+        case "Small":
+            this.addLife = 50;
+            this.addMagic = 30;
+            break;
+        default:
+            this.addLife = 0; // Default case if invalid size
+            this.addMagic = 0;
+            break;
+		}
+	}
+	
+	public String getSize() {
+        return size;
+    }
+
+    public int getAddLife() {
+        return addLife;
+    }
+
+    public int getAddMagic() {
+        return addMagic;
+    }
+    
+    
+}
